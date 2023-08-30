369b69f1ed7c57f2161cbe9db055c7ddf15c2002
Daniel <palm6991@gmail.com>
2023-08-30 19:40:23 +0200

    edited-start.sh file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# drum-n-code
+## Starting the Project 🚀
+
+To get the project up and running, simply execute the following command:
+
+\```bash
+sh start.sh
+\```
+
+Upon executing, the project will automatically set itself up, and the database will be populated with test data.
+
+> 🚨 **Important**: Ensure you run the following commands **not in the docker container, but locally** within the project folder:
+
+\```bash
+yarn install
+yarn watch
+\```
+
+### User Credentials 🖋
+
+For testing purposes, you can log in using the following user credentials:
+
+- **User 1**:
+    - **Email**: user@user.com
+    - **Password**: test
+
+- **User 2**:
+    - **Email**: test@test.com
+    - **Password**: test
--- start.sh
+++ start.sh
@@ -5,10 +5,16 @@ echo "Launching containers..."
 docker-compose up -d
 docker-compose logs -f &
 
+# Replace DATABASE_URL value with container name
+CONTAINER_DB=$(docker-compose ps -q mysql | xargs docker inspect -f '{{ .Name }}' | cut -c2-)
+sed -i.bak "s/db_container/$CONTAINER_DB/g" ./project/.env
+
 
 # Running commands in the php container and outputting logs
 echo "Installing Composer Dependencies..."
 docker exec -it $(docker ps -qf "name=php") sh -c "composer install"
+echo "Generating JWT Keypair..."
+docker exec -it $(docker ps -qf "name=php") sh -c "php bin/console lexik:jwt:generate-keypair"
 echo "Performing DB Migrations..."
 docker exec -it $(docker ps -qf "name=php") sh -c "php bin/console doctrine:migrations:migrate --no-interaction"
 
